55883aeb744aa58712e4c0aaf4590c2c024264e4
Gabriel Zavarse <zavarsegabriel@gmail.com>
2024-12-10 00:25:25 +0000

    landingpage

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,80 @@
 import React from "react";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+import Footer from "./footer";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const cards = [
+    {
+      title: "Card title 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      buttonText: "Find Out More!",
+      imgSrc: "https://via.placeholder.com/500x325",
+    },
+    {
+      title: "Card title 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      buttonText: "Find Out More!",
+      imgSrc: "https://via.placeholder.com/500x325",
+    },
+    {
+      title: "Card title 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      buttonText: "Find Out More!",
+      imgSrc: "https://via.placeholder.com/500x325",
+    },
+    {
+      title: "Card title 4",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      buttonText: "Find Out More!",
+      imgSrc: "https://via.placeholder.com/500x325",
+    },
+  ];
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <>
+
+      <Navbar />
+      <div className="container my-4">
+        <Jumbotron />
+        <div className="row">
+          {cards.map((card, index) => (
+            <div className="col-md-3 mb-4" key={index}>
+              <Card
+                title={card.title}
+                description={card.description}
+                buttonText={card.buttonText}
+                imgSrc={card.imgSrc}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+  <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
 };
 
 export default Home;
